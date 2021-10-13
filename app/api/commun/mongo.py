00a9c980f08ba217b2d60cf2b6ea15e9e751e8bc
ClimenteA <climente.alin@gmail.com>
2021-10-13 15:02:54 +0300

    trying to make prod work

--- app/api/commun/mongo.py
+++ app/api/commun/mongo.py
@@ -7,11 +7,11 @@ from contextlib import contextmanager
 
 @contextmanager
 def collection(collection_name:str):
-    """ This assures we have maximum 2 connections open to mongo """
-    conn = MongoClient(os.environ['MONGO_CONNECTION_STRING'])
-    col = conn[os.environ['MONGO_DATABASE_NAME']][collection_name]
-    try:
-        yield col
-    finally:
-        conn.close()
-        
+    
+    with MongoClient(os.environ['MONGO_CONNECTION_STRING']) as conn: 
+        col = conn[os.environ['MONGO_DATABASE_NAME']][collection_name]
+        try:
+            yield col
+        finally:
+            conn.close()
+            
--- makefile
+++ makefile
@@ -4,6 +4,11 @@ dev:
 db:
 	docker-compose up --remove-orphans --force-recreate mongo_db_local
 
-
 api:
 	docker-compose up --remove-orphans --force-recreate fast_api_local
+
+prod:
+	docker-compose -f docker-compose.prod.yml up --remove-orphans --force-recreate 
+
+caddy2:
+	docker-compose -f docker-compose.prod.yml up --remove-orphans --force-recreate caddy_prod
